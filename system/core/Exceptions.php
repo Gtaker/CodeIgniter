c2fb5670bd32d332849db989c1ab9f4c56782039
Gtacer <459472218@qq.com>
2017-11-08 22:52:15 +0800

    校正 system/core/Exceptions.php（异常类），并删除原英文注释

--- system/core/Exceptions.php
+++ system/core/Exceptions.php
@@ -40,8 +40,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Exceptions Class
- * 异常处理类
+ * 异常类
  *
  * @package		CodeIgniter
  * @subpackage	Libraries
@@ -52,15 +51,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class CI_Exceptions {
 
 	/**
-	 * Nesting level of the output buffering mechanism
-     * 输出缓存机制的嵌套等级
+     * （当前）输出缓存机制的嵌套等级
 	 *
 	 * @var	int
 	 */
 	public $ob_level;
 
 	/**
-	 * List of available error levels
      * 可用的错误等级列表
 	 *
 	 * @var	array
@@ -81,7 +78,6 @@ class CI_Exceptions {
 	);
 
 	/**
-	 * Class constructor
      * 构造函数
 	 *
 	 * @return	void
@@ -89,23 +85,20 @@ class CI_Exceptions {
 	public function __construct()
 	{
 		$this->ob_level = ob_get_level();
-		// Note: Do not log messages from this constructor.
-        // 提示：不要在这个构造函数中记录信息
+        // 提示：不要从这个构造函数记录消息
 	}
 
 	// --------------------------------------------------------------------
 
 	/**
-	 * Exception Logger
      * 异常记录器
 	 *
-	 * Logs PHP generated error messages
      * 记录 PHP 生成的错误信息
 	 *
-	 * @param	int	$severity	Log level   日记等级
-	 * @param	string	$message	Error message   错误信息
-	 * @param	string	$filepath	File path   文件路径
-	 * @param	int	$line		Line number     行数
+	 * @param	int	$severity	记录等级
+	 * @param	string	$message	错误信息
+	 * @param	string	$filepath	文件路径
+	 * @param	int	$line		行数
 	 * @return	void
 	 */
 	public function log_exception($severity, $message, $filepath, $line)
@@ -117,13 +110,12 @@ class CI_Exceptions {
 	// --------------------------------------------------------------------
 
 	/**
-	 * 404 Error Handler
      * 404 错误处理
 	 *
 	 * @uses	CI_Exceptions::show_error()
 	 *
-	 * @param	string	$page		Page URI    页面 URI
-	 * @param 	bool	$log_error	Whether to log the error    是否记录错误
+	 * @param	string	$page		页面 URI
+	 * @param 	bool	$log_error	是否记录错误
 	 * @return	void
 	 */
 	public function show_404($page = '', $log_error = TRUE)
@@ -139,8 +131,7 @@ class CI_Exceptions {
 			$message = 'The page you requested was not found.';
 		}
 
-		// By default we log this, but allow a dev to skip it
-        // 默认的我们会将异常记录下来，但是也允许通过传入的参数来跳过这一步
+        // 默认的我们会将异常记录下来，但是也允许通过修改传入的参数来跳过这一步
 		if ($log_error)
 		{
 			log_message('error', $heading.': '.$page);
@@ -153,19 +144,16 @@ class CI_Exceptions {
 	// --------------------------------------------------------------------
 
 	/**
-	 * General Error Page
      * 普通错误页面
 	 *
-	 * Takes an error message as input (either as a string or an array)
-	 * and displays it using the specified template.
      * 根据指定的模版显示传入的错误信息（字符串或数组）。
 	 *
-	 * @param	string		$heading	Page heading    错误标题
-	 * @param	string|string[]	$message	Error message   错误信息
-	 * @param	string		$template	Template name   模版名
-	 * @param 	int		$status_code	(default: 500)  （默认：500）
+	 * @param	string		$heading	错误标题
+	 * @param	string|string[]	$message	错误信息
+	 * @param	string		$template	模版名
+	 * @param 	int		$status_code	（默认：500）
 	 *
-	 * @return	string	Error page output
+	 * @return	string	错误页面的输出（注：以字符串形式返回）
 	 */
 	public function show_error($heading, $message, $template = 'error_general', $status_code = 500)
 	{
@@ -238,13 +226,12 @@ class CI_Exceptions {
 	// --------------------------------------------------------------------
 
 	/**
-	 * Native PHP error handler
      * 原生 PHP 错误处理
 	 *
-	 * @param	int	$severity	Error level     错误等级
-	 * @param	string	$message	Error message   错误信息
-	 * @param	string	$filepath	File path   文件路径
-	 * @param	int	$line		Line number     行数
+	 * @param	int	$severity	错误等级
+	 * @param	string	$message	错误信息
+	 * @param	string	$filepath	文件路径
+	 * @param	int	$line		行数
 	 * @return	void
 	 */
 	public function show_php_error($severity, $message, $filepath, $line)
@@ -257,7 +244,6 @@ class CI_Exceptions {
 
 		$severity = isset($this->levels[$severity]) ? $this->levels[$severity] : $severity;
 
-		// For safety reasons we don't show the full file path in non-CLI requests
         // 出于对安全性的考虑，我们不会在非CLI请求下显示完整的文件路径
 		if ( ! is_cli())
 		{
