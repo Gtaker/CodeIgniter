8e4d31d30c15c1adcd8730f1c6c0152b24a30579
Gtacer <459472218@qq.com>
2017-11-12 21:52:29 +0800

    application/controllers/Welcome.php（示例控制器）文件，翻译初稿完成

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,17 +5,22 @@ class Welcome extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
+     * 该控制器的引导页。
 	 *
 	 * Maps to the following URL
+     * 影射到以下 URL 地址
 	 * 		http://example.com/index.php/welcome
-	 *	- or -
+	 *	- 或 -
 	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
+	 *	- 或 -
 	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
+     * 如果在 config/routes.php 中将此控制器设置为默认控制器，
+     * 则该方法映射到 http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
+     * 所以任何其他不带下划线前缀的公共方法都将映射到 /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
